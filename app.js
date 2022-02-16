2bae8e84c6306b86472b7ad505da8f93b60ff5d6
REX <cssmith431@yahoo.com>
2022-02-16 13:36:47 -0500

    inquirer installed

---
+++ app.js
@@ -0,0 +1,21 @@
+//const readmeDataArgs = process.argv.slice(2, process.length);
+
+
+//const printReadmeData = readmeDataArr => {
+  //  for (let i = 0; i < readmeDataArr.length; i+=1) {
+    //  console.log(readmeDataArr[i]);
+    //}
+
+    //console.log('======');
+
+    //readmeDataArr.forEach((readmeItem) =>{
+      //  console.log(readmeItem)
+    //});
+
+  //};
+
+//printReadmeData(readmeDataArgs)
+
+const generateReadme = () => 'Name: Cooper, Github:cssmith';
+
+console.log(generateReadme())
