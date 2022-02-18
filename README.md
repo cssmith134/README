612807d91fd16e6d2a0f3e0c515a9966e5bf0766
REX <cssmith431@yahoo.com>
2022-02-18 14:21:08 -0500

    able to append data onto readme page

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 
     
+    ## Title
+    vsves
+
+    ## Description
+    vev
+
+    ##Installation
+    vesv
+
+    ## Usage 
+    ves
+
+
     Name: undefined
     Github: undefined
     
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,11 +1,26 @@
 
 
 module.exports = templateData => {
+
+const {description, title, installation, usage} = templateData;
  console.log(templateData)
 
 
     return `
     
+    ## Title
+    ${templateData.title}
+
+    ## Description
+    ${templateData.description}
+
+    ##Installation
+    ${templateData.installation}
+
+    ## Usage 
+    ${templateData.usage}
+
+
     Name: ${templateData.name}
     Github: ${templateData.github}
     `;
